7ae93ff64c1590d69ac51dabfdaa22f9b5087194
zastasja <anastasia_zh@bk.ru>
2022-09-14 17:40:22 +0300

    added HW3-2.py

---
+++ HW3-2.py
@@ -0,0 +1,11 @@
+# 3.2 Дан список list_1 = ['Hi', 'ananas', 2, None, 75, 'pizza', 36, 100]
+#   - получите сумму всех чисел,
+#   - распечатайте все строки, где есть буква 'a'
+
+list_1 = ['Hi', 'ananas', 2, None, 75, 'pizza', 36, 100]
+my_num = ([x for x in list_1 if isinstance(x, (int, float))])
+print(sum(my_num))
+
+my_word = ([y for y in list_1 if isinstance(y, str)])
+print(my_word)
+# доделать: выбрать слова с буквой 'a'
