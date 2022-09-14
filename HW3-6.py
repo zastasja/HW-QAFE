1c4138c1be8a15d30cfe01e00aa1f7f7c05fbf19
zastasja <anastasia_zh@bk.ru>
2022-09-14 17:15:31 +0300

    added HW3-6.py

---
+++ HW3-6.py
@@ -0,0 +1,9 @@
+#3.6. Найдите сумму всех значений в словаре my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}
+
+my_dictionary = {
+    'num1': 375,
+    'num2': 567,
+    'num3': -37,
+    'num4': 21
+}
+print(sum(my_dictionary.values()))
