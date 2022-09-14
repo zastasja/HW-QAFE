c1422ef1de9c17dbdc169338763f604394f43ad5
zastasja <anastasia_zh@bk.ru>
2022-09-14 17:12:11 +0300

    added HW3-5.py

---
+++ HW3-5.py
@@ -0,0 +1,10 @@
+film = {
+    'title': 'Hackers',
+    'director': 'Iain Softley',
+    'year': 1995,
+    'budget': 20000000,
+    'main_actor': 'Jonny Lee Miller',
+    'slogan': 'Hack The Planet!'
+}
+
+print(film)
