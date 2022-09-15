a24806798b079bbc0d5822e714e8f7640e17a0e9
zastasja <anastasia_zh@bk.ru>
2022-09-15 14:00:27 +0300

    updated HW3-4.py

--- lesson 3/HW3-4.py
+++ lesson 3/HW3-4.py
@@ -1,9 +1,12 @@
-family_1 = list(input())
-family_2 = list(input())
+family_1 = input()
+family_2 = input()
 
-if len(family_1) > len(family_2):
+f1 = list(family_1.split(","))
+f2 = list(family_2.split(","))
+
+if len(f1) > len(f2):
     print('First family bigger')
-elif len(family_2) > len(family_1):
+elif len(f2) > len(f1):
     print('Second family bigger')
 else:
     print('Equal')
