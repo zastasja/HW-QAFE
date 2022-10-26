9e0e8f36142b26ad6e932a96904eb5b1212e8326
zastasja <anastasia_zh@bk.ru>
2022-10-26 11:20:26 +0300

    added api tests

--- lesson 8/test_api.py
+++ lesson 8/test_api.py
@@ -1,9 +1,7 @@
 import requests
 import pytest
-import time
 
 url1 = 'https://jsonplaceholder.typicode.com/'
-url2 = 'https://playground.learnqa.ru/api/'
 HTTP_code_OK = 200
 HTTP_code_OK2 = 201
 
@@ -34,3 +32,19 @@ def test_post_in_posts():
     response_data = response.json()
     expected_title = 'test title'
     assert response_data['title'] == expected_title, 'wrong title'
+
+
+def get_all_names():
+    response = requests.get(f'{url1}users/')
+    response_data = response.json()
+    name_list = []
+    for i, name in enumerate(response_data):
+        name_list.append((i+1, response_data[i]['name']))
+    return name_list
+
+
+@pytest.mark.parametrize('userid, expected_name', get_all_names())
+def test_get_all_users_name(userid, expected_name):
+    response = requests.get(f'{url1}users/{userid}')
+    response_data = response.json()
+    assert response_data['name'] == expected_name, 'wrong name'
---
+++ lesson 8/test_api2.py
@@ -0,0 +1,7 @@
+import requests
+import pytest
+import time
+
+url = 'https://playground.learnqa.ru/api/'
+HTTP_code_OK = 200
+HTTP_code_OK2 = 201
